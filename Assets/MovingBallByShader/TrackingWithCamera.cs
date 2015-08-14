fa5760d294fdca56fb441acdcd87f343cb2086f8
Aquaware <aquawareCo@gmail.com>
2015-08-14 23:28:08 +0900

    Moving Ball Added
    Moving Ball by Shader

---
+++ Assets/MovingBallByShader/TrackingWithCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingWithCamera : MonoBehaviour {
+
+	public Material material;
+	public string shaderPositionName = "_Position";
+	public float distance = 10.0f;
+	
+	private Camera thisCamera;
+	
+	
+	// Use this for initialization
+	void Start () {
+		thisCamera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var screen = Input.mousePosition;
+        screen.z = distance;
+        var world = thisCamera.ScreenToWorldPoint(screen);
+		Debug.Log(world);
+        material.SetVector(shaderPositionName, world);
+	}
+}
